1c0d0cf1f8bc2ae16f9979776c691f93ab210a63
apoorvgupt479 <apoorvgupta479@gmail.com>
2021-01-04 19:35:17 +0530

    game files

---
+++ Box.js
@@ -0,0 +1,31 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        isStatic:false,
+        'restitution':0.5,
+        'friction':0.5,
+        'density':0.5
+
+    }
+    this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+
+    World.add(world, this.body);
+  }
+  display(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    strokeWeight(4);
+    stroke(100,200,100);
+    //fill(100,100,200);
+    rect(0,0,this.width,this.height);
+    pop();
+  }
+}
